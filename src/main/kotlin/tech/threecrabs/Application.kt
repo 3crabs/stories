0b3899874b0e207e4f100e88a404ab3b987d90f9
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2020-12-04 01:25:32 +0700

    add open-api docs

--- src/main/kotlin/tech/threecrabs/Application.kt
+++ src/main/kotlin/tech/threecrabs/Application.kt
@@ -1,11 +1,20 @@
 package tech.threecrabs
 
-import io.micronaut.runtime.Micronaut.build
+import io.micronaut.runtime.Micronaut
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 
-fun main(args: Array<String>) {
-    build()
-        .args(*args)
-        .packages("tech.threecrabs")
-        .start()
+@OpenAPIDefinition(
+    info = Info(
+        title = "Stories",
+        version = "1.0.0",
+        description = "Приложение для пользовательских историй"
+    )
+)
+object Application {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.run(Application.javaClass)
+    }
 }
-
