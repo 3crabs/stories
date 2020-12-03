1dd35d7a94402f5519c2b185ce15512d70a0a771
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2020-12-04 00:56:01 +0700

    add TEXT_HTML and ModelAndView in IndexClient.kt

--- src/test/kotlin/tech/threecrabs/IndexClient.kt
+++ src/test/kotlin/tech/threecrabs/IndexClient.kt
@@ -1,12 +1,14 @@
 package tech.threecrabs
 
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.views.ModelAndView
 
 @Client("/")
 interface IndexClient {
 
-    @Get
-    fun index(): HttpResponse<String>
+    @Get(processes = [MediaType.TEXT_HTML])
+    fun index(): HttpResponse<ModelAndView<String>>
 }
