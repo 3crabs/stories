f9754fef6e0279a62311f0267c0e728d7d792743
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2020-12-04 00:55:14 +0700

    add HttpResponse in IndexController.kt

--- src/main/kotlin/tech/threecrabs/IndexController.kt
+++ src/main/kotlin/tech/threecrabs/IndexController.kt
@@ -1,5 +1,6 @@
 package tech.threecrabs
 
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
@@ -11,7 +12,7 @@ class IndexController {
 
     @Get(processes = [MediaType.TEXT_HTML])
     @View("index")
-    fun index(): ModelAndView<String> {
-        return ModelAndView("index", null)
+    fun index(): HttpResponse<ModelAndView<String>> {
+        return HttpResponse.ok(ModelAndView("index", null))
     }
 }
