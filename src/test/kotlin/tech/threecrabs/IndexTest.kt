e3244d1f65984098567cd79affb77324c8b61f12
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2020-12-04 00:56:15 +0700

    rm old tests

--- src/test/kotlin/tech/threecrabs/IndexTest.kt
+++ src/test/kotlin/tech/threecrabs/IndexTest.kt
@@ -1,10 +1,6 @@
 package tech.threecrabs
 
 import io.kotest.core.spec.style.FeatureSpec
-import io.kotest.data.forAll
-import io.kotest.data.row
-import io.kotest.matchers.booleans.shouldBeTrue
-import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.micronaut.http.HttpStatus
 import io.micronaut.test.extensions.kotest.annotation.MicronautTest
@@ -21,20 +17,6 @@ class IndexTest : FeatureSpec() {
 
             scenario("существует") {
                 indexClient.index().status shouldBe HttpStatus.OK
-            }
-
-            scenario("тело ответа не пустое") {
-                indexClient.index().body().shouldNotBeNull()
-            }
-
-            scenario("есть ссылки документации") {
-                forAll(
-                    row("<a href='/swagger-ui/index.html'>swagger-ui</a>"),
-                    row("<a href='/redoc/index.html'>redoc</a>"),
-                    row("<a href='/rapidoc/index.html'>rapidoc</a>")
-                ) { link ->
-                    indexClient.index().body()!!.contains(link).shouldBeTrue()
-                }
             }
         }
     }
